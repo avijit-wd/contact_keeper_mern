851de07851e1e9ea4eb4328dd334d70997317341
Avijit Biswas <avijitb440@gmail.com>
2019-12-01 14:31:58 +0530

    Completed backend coding

--- routes/contacts.js
+++ routes/contacts.js
@@ -75,7 +75,18 @@ router.put("/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-router.delete("/:id", (req, res) => {
-  res.send("Delete contact");
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact) return res.status(404).json({ msg: "Contact not found" });
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Not Authorised" });
+    }
+    await Contact.findByIdAndRemove(req.params.id);
+    res.json({ msg: "Contact removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 module.exports = router;
