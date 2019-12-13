4697e89f229cfc19753cc2c7529b3a622f95a0bf
Avijit Biswas <avijitb440@gmail.com>
2019-12-13 11:23:13 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const pat = require("path");
 const app = express();
+
 const connectDB = require("./config/db");
 connectDB();
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the contact Keeper API..." });
-});
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(pat.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
